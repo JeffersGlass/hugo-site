8bc5016f451c6655401675b80b188ae286575149
Jeff Glass <glass.jeffrey@gmail.com>
2021-11-29 07:51:21 -0600

    Blog cards automatically resize on small screens

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
   },
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      width: {
+        '20-1': '19%',
+      }
+    },
   },
   variants: {
     margin: ['first'],
