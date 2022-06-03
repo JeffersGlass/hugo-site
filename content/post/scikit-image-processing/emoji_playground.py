8c2c0dcf23df0d7b5d62bf03ad5870f8ea3c6ad2
Jeff Glass <glass.jeffrey@gmail.com>
2022-06-03 15:56:01 -0500

    Develop short demo for emoji transform

---
+++ content/post/scikit-image-processing/emoji_playground.py
@@ -0,0 +1,97 @@
+from PIL import Image
+
+from js import document, console, Uint8Array, window, File
+from pyodide import create_proxy
+from pyodide.http import pyfetch
+import asyncio
+import io
+import numpy as np
+from numpy import asarray
+
+current_emoji = "🤖"
+current_filter_name = "swirl"
+
+async def get_emoji_bytes(url: str):
+    response = await pyfetch(url)
+    if response.status == 200:
+        return await response.bytes()
+
+async def _select_emoji_and_display(e):
+    current_emoji = e.target.value
+    await _fetch_and_display()
+
+async def _select_filter_and_display():
+    current_filter_name = e.target.value
+    await _fetch_and_display()
+
+async def _fetch_and_display():
+    # Get an emoji image and fetch it:
+    emoji = current_emoji
+    emoji_code = "-".join(f"{ord(c):x}" for c in emoji).upper()
+    url = f"https://raw.githubusercontent.com/hfg-gmuend/openmoji/master/color/618x618/{emoji_code}.png"
+    console.log(f"Getting emoji {emoji} with value {emoji_code} at url {url}")
+
+
+    #BytesIO wants a bytes-like object, so convert to bytearray first
+    bytes_list = bytearray(await get_emoji_bytes(url))
+    my_bytes = io.BytesIO(bytes_list) 
+
+    #Create PIL image from BytesIO 
+    my_original_image = Image.open(my_bytes)
+    my_image = Image.open(my_bytes)
+
+    #Convert to an np-array to allow for processing
+    my_array = np.array(my_image.convert()) # convert() is key, as these images use a pallete!!
+
+    # -------- Do image processing here ------
+
+    from skimage.transform import swirl
+    my_array = swirl(my_array, rotation = 0, strength = 15, radius = 300)
+
+    # -------- End image processing -----------
+
+    #convert back to Pillow image:
+    if my_array[row:= 0][column:= 0][red:= 0] < .99:
+        # Many transforms represent RGB as floats in the range 0-1, which pillow does not like
+        # This converts their values back to 0-255
+        my_image = Image.fromarray((my_array*255).astype(np.uint8)) 
+    else:
+        my_image = Image.fromarray(my_array)
+
+    #Export image from Pillow as bytes to get to Javascript
+    my_processed_stream = io.BytesIO()
+    my_image.save(my_processed_stream, format="PNG")
+
+    #Create a JS File object with our data and the proper mime type
+    processed_image_file = File.new([Uint8Array.new(my_processed_stream.getvalue())], "new_image_file.png", {type: "image/png"})
+    original_image_file = File.new([Uint8Array.new(my_bytes.getvalue())], "new_image_file.png", {type: "image/png"})
+
+    #remove all children from divs:
+    remove_all_children("new_image")
+    remove_all_children("original_image")
+
+    #Create new tags and insert into page
+    new_image = document.createElement('img')
+    new_image.src = window.URL.createObjectURL(processed_image_file)
+    document.getElementById("new_image").appendChild(new_image)
+
+    original_image = document.createElement('img')
+    original_image.classList.add("w-auto")
+    original_image.src = window.URL.createObjectURL(original_image_file)
+    document.getElementById("original_image").appendChild(original_image)
+
+select_emoji_and_display = create_proxy( _select_emoji_and_display)
+document.getElementById("emoji-selector").addEventListener("change",  select_emoji_and_display)
+
+select_filter_and_display = create_proxy( _select_filter_and_display)
+document.getElementById("filter-selector").addEventListener("change",  select_filter_and_display)
+
+def remove_all_children(parent_id: str):
+    parent = document.getElementById(parent_id)
+
+    while parent.firstChild is not None:
+        parent.removeChild(parent.firstChild) 
+
+await _fetch_and_display()
+
+x=1
