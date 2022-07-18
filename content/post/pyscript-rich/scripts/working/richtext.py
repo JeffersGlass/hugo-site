545bd16971e2c011b1ee5b8c678eb2d5904c798e
Jeff Glass <glass.jeffrey@gmail.com>
2022-07-18 16:01:20 -0500

    Improve posting about rich

--- content/post/pyscript-rich/scripts/working/richtext.py
+++ content/post/pyscript-rich/scripts/working/richtext.py
@@ -9,9 +9,35 @@ print("[u]Underlined[/u]")
 print(['Lists', 'get', 'pretty', 'printed', 1234, object()])
 print({'Dictionaries':'Get', 'PrettyPrinted?':True, 'Number': 1234})
 print("Links, like this link to [link=https://www.willmcgugan.com]William McGugan's blog[/link]!")
+print("")
 
 from rich.text import Text
 text = Text("WOW! Text objects are powerful, as is the ")
 text.stylize("bold", 0, 4)
 text.append("append() method", style="underline")
 print(text)
+
+###
+
+from rich.layout import Layout
+from rich.text import Text
+from rich.panel import Panel
+from rich.align import Align
+
+layout = Layout()
+
+layout.split_column(
+    Layout(name="upper"),
+    Layout(name="lower")
+)
+
+layout["lower"].split_row(
+    Layout(name="left"),
+    Layout(name="right"),
+)
+
+layout['upper'].update(Panel(Align(Text("This is a big ole panel huh?", style="italic"), align="center", vertical="middle"), title="Big Window"))
+layout['left'].update(Panel("[red]The lower right subpanel has no contents, so you'll see a placeholder over there >>> [/]", title="[red bold]WARNING[/]"))
+
+print(layout)
+
