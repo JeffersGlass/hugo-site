47e278d8398ea82eab19e56f88688c5dda32158c
Jeff Glass <glass.jeffrey@gmail.com>
2022-08-15 11:47:45 -0500

    Continue fleshing out post

---
+++ content/post/pyscript-rich/scripts/working/intro.py
@@ -0,0 +1,4 @@
+print("[bold]TL;DR:[/bold] This text is being [cyan1 on green]formatted[/] by the [link'https://github.com/Textualize/rich']Rich Console Formatting Library[/]")
+print("and output [u]directly[/u] via PyScript.")
+print("The REPL below is automatically formatted with RICH;")
+print("Press [italic]shift+enter[/] or click :play_button: to execute the REPL:")
