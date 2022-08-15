9e34065d46f0c607c3ec4e09d5da6f0998a15924
Jeff Glass <glass.jeffrey@gmail.com>
2022-08-15 14:39:34 -0500

    Adjust formatting

--- content/post/pyscript-rich/_richsetup.py
+++ content/post/pyscript-rich/_richsetup.py
@@ -1,12 +1,11 @@
-from array import array
+from typing import Iterable
 from rich import get_console
+from rich.segment import Segment
 import rich.jupyter
-from rich.pretty import Pretty
 from pyodide import JsException
+from sys import stdout, modules
 
 from js import console
-
-from sys import stdout, modules
 
 #Per pyodide docs, determine if we're running inside pyodide at Runtime
 def is_pyodide() -> bool:
@@ -15,7 +14,7 @@ def is_pyodide() -> bool:
 c = get_console()
 c.is_jupyter = is_pyodide #monkeypatch jupyter detection @propety
 
-def display_pyscript(segments, text: str) -> None:
+def display_pyscript(segments: Iterable[Segment], text: str) -> None:
     """Allow output of raw HTML within pyscript/pyodide"""
     html = rich.jupyter._render_segments(segments)
     stdout.write(html)
@@ -27,10 +26,11 @@ rich.jupyter.display = display_pyscript
 print = rich.jupyter.print
 
 class output_buffer():
+    """ A (inefficient) buffer to capture stdout to a string """
     def __init__(self):
         self._internal_buffer = ''
 
-    def write(self, value):
+    def write(self, value: any):
         if len(self._internal_buffer):
             self.internal_buffer += '<br>'
         self._internal_buffer += value
@@ -42,6 +42,7 @@ from contextlib import contextmanager
 
 @contextmanager
 def stdout_to_buffer(el:Element):
+    """ A context manager to manage an output_buffer, writes to an Element on closure"""
     global stdout #sys.stdout
     _old_stdout = stdout
     stdout = output_buffer()
@@ -52,7 +53,8 @@ def stdout_to_buffer(el:Element):
         stdout = _old_stdout 
 
 #Allow Element.write() to take an object from rich
-def newWrite(self, value, append=False):
+def newWrite(self, value, append=False) -> None:
+    """ A Monkeypatched version of Pyscript's Element.write(), auto-transforming Rich objects and rendering standard objects. """
     if hasattr(value, '__rich_console__'):
         console.warn(f"Using newWrite() __rich_console__ on {str(value)[:min(30, len(str(value)))]}...with type {type(value)}")
         self._write(rich.jupyter._render_segments(value.__rich_console__(c, c.options)), append)
