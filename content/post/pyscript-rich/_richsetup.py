529a28cc2219ed5721664d4c94edb2674817f35c
Jeff Glass <glass.jeffrey@gmail.com>
2022-08-15 13:30:32 -0500

    Add context manager for element.write

--- content/post/pyscript-rich/_richsetup.py
+++ content/post/pyscript-rich/_richsetup.py
@@ -1,7 +1,10 @@
+from array import array
 from rich import get_console
 import rich.jupyter
 from rich.pretty import Pretty
 from pyodide import JsException
+
+from js import console
 
 from sys import stdout, modules
 
@@ -17,11 +20,36 @@ def display_pyscript(segments, text: str) -> None:
     html = rich.jupyter._render_segments(segments)
     stdout.write(html)
 
-#monkeypatch jupyter display method to write processed HTML to stdout
+#patch jupyter display method to write processed HTML to stdout
 rich.jupyter.display = display_pyscript 
 
 #Overwrite print with Rich Jupyter print function
 print = rich.jupyter.print
+
+class output_buffer():
+    def __init__(self):
+        self._internal_buffer = ''
+
+    def write(self, value):
+        if len(self._internal_buffer):
+            self.internal_buffer += '<br>'
+        self._internal_buffer += value
+
+    def read(self):
+        return self._internal_buffer
+
+from contextlib import contextmanager
+
+@contextmanager
+def stdout_to_buffer(el:Element):
+    global stdout #sys.stdout
+    _old_stdout = stdout
+    stdout = output_buffer()
+    try:
+        yield stdout
+    finally:
+        el._write(stdout.read(), append=True)
+        stdout = _old_stdout 
 
 #Allow Element.write() to take an object from rich
 def newWrite(self, value, append=False):
@@ -37,11 +65,8 @@ def newWrite(self, value, append=False):
         self._write(value, append)
     else:
         console.warn(f"Using newWrite() with Pretty interpretted on {str(value)[:min(30, len(str(value)))]}... with type {type(value)}")
-        c.print(value)
-        
-        #self.write(value.__rich_console__(c, c.options), append)
-        #value.__rich__ = value.__rich_console__
-        #self._write(rich.jupyter._render_segments(value.__rich__(c, c.options)), append)
+        with stdout_to_buffer(self):
+            c.print(value)
 
 Element._write = Element.write
 Element.write = newWrite
