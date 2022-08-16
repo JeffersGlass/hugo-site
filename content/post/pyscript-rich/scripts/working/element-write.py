21c4492e0725c60a30887cdee5d44dccd35fefcb
Jeff Glass <glass.jeffrey@gmail.com>
2022-08-15 21:23:05 -0500

    Add syntax highlighting

--- content/post/pyscript-rich/scripts/working/element-write.py
+++ content/post/pyscript-rich/scripts/working/element-write.py
@@ -5,7 +5,7 @@ el = Element("write-to-me")
 text = Text.from_markup("[red bold]Rich objects[/] can be output with [i]Element.write()[/i]")
 el.write(text, append=True)
 
-data = {"Question": "Can standard Python Objects?", "Answers": ["Yes!", "They Can!", "How neat!"]}
+data = {"Question": "What about standard Python Objects?", "Answers": ["Yes!", "They Can!", "How neat!"]}
 el.write(data, append=True)
 
 
