5b234e4672cd96be886ec4322f124ab46e6b61ff
Jeff Glass <glass.jeffrey@gmail.com>
2022-08-16 13:37:00 -0500

    Several independent demos working

--- content/post/pyscript-rich-test/liverunner.py
+++ content/post/pyscript-rich-test/liverunner.py
@@ -1,13 +1,29 @@
 import asyncio
-from rich import get_console
-from rich.live import Live
-from rich.panel import Panel
+from random import random, choice, randint
+from rich.table import Table
+from rich.emoji import Emoji
+from datetime import datetime
+from faker import Faker
+#from livepatch import Live
 
-c = get_console()
-el = Element("live")
+fake = Faker()
 
-while(True):
-    with Live(my_text, auto_refresh=False) as live_panel:
-        await asyncio.sleep(1)
-        live_panel.update(my_text)
-        console.log("Updated panel")
+print('[i]Source (Partial)[/]')
+
+from rich.syntax import Syntax
+
+syntax = Syntax.from_path('./liverunner.py', padding=1, line_range=(18, None), line_numbers=True)
+print(syntax)
+
+table = Table(width = 80)
+table.add_column("Time", width=5)
+table.add_column("Source", width=15)
+table.add_column("Destination", width=15)
+
+with Live(table, "live-div"):
+    for _ in range(12):
+        await asyncio.sleep(.4 + random() * .7)
+        time = datetime.now().strftime('%S.%f')
+        time = time[:min(len(time), 5)]
+        source, dest = fake.ipv4(), fake.ipv4()
+        table.add_row(time, source, dest)
