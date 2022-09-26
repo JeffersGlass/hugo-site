c2f4fbbceb04d4ff25995a51c4d1b037f2a39043
Jeff Glass <glass.jeffrey@gmail.com>
2022-09-26 15:31:34 -0500

    Clean up post

--- content/post/pyscript-rich/scripts/working/intro.py
+++ content/post/pyscript-rich/scripts/working/intro.py
@@ -1,4 +1,4 @@
-print("[bold]TL;DR:[/bold] This text is being [b]formatted[/b] by the [link https://github.com/Textualize/rich]Rich Console Formatting Library[/]")
-print("and output with [yellow1 on grey15]<PyScript>[/]. There are [b link #examples]#more examples below :point_down:[/]")
+print("[bold]This text[/bold] is being [b]formatted[/b] by the [link https://github.com/Textualize/rich]Rich Console Formatting Library[/]")
+print("and output with [yellow1 on grey15]<PyScript>[/]. There's a [b link ../../project/richdemo]whole page of examples :link:[/]")
 print("The REPL below is automatically formatted with RICH;")
 print("Press [italic]shift+enter[/] or click :play_button: to execute the REPL:")
