79eb868b108ffb2d23a0c98ff2216ab56bf61dcb
Jeff Glass <glass.jeffrey@gmail.com>
2022-10-20 14:00:28 -0500

    Work on post

--- content/post/pyscript-asyncio/bad_add.py
+++ content/post/pyscript-asyncio/bad_add.py
@@ -1,7 +1,8 @@
 import asyncio
 async def add_slowly():
     Element("out").write("One plus two is... wait for it...")
-    await asyncio.sleep(1)
+    await asyncio.sleep(5)
     Element("out").write(1+2, append=True)
 
+#This isn't normally possible:
 await add_slowly()
