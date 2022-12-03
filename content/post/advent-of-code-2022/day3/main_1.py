f2a023e9fcffd7f7b56ec64127ac266d5800addf
Jeff Glass <glass.jeffrey@gmail.com>
2022-12-03 09:06:38 -0600

    Day 3 part 1

---
+++ content/post/advent-of-code-2022/day3/main_1.py
@@ -0,0 +1,33 @@
+import string
+import sys
+
+charValue = {s: index + 1 for index, s in enumerate(string.ascii_lowercase)} |\
+            {s: index + 27 for index, s in enumerate(string.ascii_uppercase)}
+
+def prioritySum(data):
+    sum = 0    
+    for line in data:
+        midpoint = int(len(line)/2)
+        first = set(line[:midpoint])
+        second = set(line[midpoint:])
+        sum += charValue[(first & second).pop()]
+    return sum
+
+if 'pyodide' in sys.modules:
+    import js
+    def main_day3_1():
+        js.console.log("Main day 3_1")
+        data = get_input('day3_1').split('\n')
+        js.console.log(data)
+        display(f"{prioritySum(data)= }",
+            target="day3_1-output",
+            append=False)
+
+elif __name__ == "__main__":
+    with open("input.txt", "r") as fp:
+        data = fp.read()
+    result = prioritySum(data.split('\n'))
+    print(f"{result= }")
+
+
+    
