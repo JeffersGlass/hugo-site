f1d4ab37d20676facef01ddc0a38481985eae164
Jeff Glass <glass.jeffrey@gmail.com>
2022-12-06 07:37:07 -0600

    Day 6

--- content/post/advent-of-code-2022/day6/main_1.py
+++ content/post/advent-of-code-2022/day6/main_1.py
@@ -5,14 +5,16 @@ def findIndexAllDifferent(input_stream, n):
     window = deque(maxlen=n)
     for index, token in enumerate(input_stream):
         window.append(token)
-        print(window)
         if index + 1 >= n and len(set(window)) >= n:
-            #print(f"{len(set(window))} {set(window)}")
             return index + 1
-
+    return -1
 
 if 'pyodide' in sys.modules:
-    pass
+    def main_day6_1():
+        data = get_input('day6_1')
+        display(f"{findIndexAllDifferent(data, 4)= }",
+            target="day6_1-output",
+            append=False)
 else:
     with open("input.txt", "r") as fp:
         data = fp.read().strip('\n')
