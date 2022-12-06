09781ada03af7ab84e1173a406eeeee4e06e0c70
Jeff Glass <glass.jeffrey@gmail.com>
2022-12-06 07:37:07 -0600

    Day 6

--- content/post/advent-of-code-2022/day6/main_2.py
+++ content/post/advent-of-code-2022/day6/main_2.py
@@ -1,20 +1,21 @@
 from collections import deque
 import sys
 
-def findIndexAllDifferent_5_2(input_stream, n):
+def findIndexAllDifferent_6_2(input_stream, n):
     window = deque(maxlen=n)
     for index, token in enumerate(input_stream):
         window.append(token)
-        print(window)
         if index + 1 >= n and len(set(window)) >= n:
-            #print(f"{len(set(window))} {set(window)}")
             return index + 1
-
+    return -1
 
 if 'pyodide' in sys.modules:
-    pass
+    def main_day6_2():
+        data = get_input('day6_2')
+        display(f"{findIndexAllDifferent_6_2(data, 14)= }",
+            target="day6_2-output",
+            append=False)
 else:
     with open("input.txt", "r") as fp:
         data = fp.read().strip('\n')
-    print(findIndexAllDifferent_5_2(data, 14))
-    
+    print(findIndexAllDifferent_6_2(data, 14))
