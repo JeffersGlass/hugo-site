91fd57b7491bbecc8f9d9e04c580492e85b5d6a1
Jeff Glass <glass.jeffrey@gmail.com>
2022-12-09 08:05:38 -0600

    Day 9 part 1

---
+++ content/post/advent-of-code-2022/day9/main_1.py
@@ -0,0 +1,42 @@
+from typing import NamedTuple
+import sys
+
+class Point(NamedTuple):
+    x: int
+    y: int
+
+direction_to_vector = {
+    "R": Point(1, 0),
+    "L": Point(-1, 0),
+    "U": Point(0, 1),
+    "D": Point(0, -1),
+}
+
+def solve_9_1(data):
+    head = Point(0, 0)
+    tail = Point(0, 0)
+    tail_visited = {tail}
+
+    for line in data.split('\n'):
+        direction, quantity = line.split(" ")
+        diff = direction_to_vector[direction]
+        for _ in range(int(quantity)):
+            prev_head = head
+            head = Point(head.x + diff.x, head.y + diff.y)
+            if too_far(head, tail):
+                tail = prev_head
+                tail_visited.add(tail)
+
+    return len(tail_visited)
+
+def too_far(head, tail):
+    return abs(head.x - tail.x) > 1 or abs(head.y - tail.y) > 1
+
+if 'pyodide' in sys.modules:
+    pass
+elif __name__ == '__main__':
+    with open ("input.txt", "r") as fp:
+        data = fp.read()
+
+    print(f"{solve_9_1(data)}")
+
