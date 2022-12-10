1d33edfff8ca20fbad9cd3940ed2e165d5c909f6
Jeff Glass <glass.jeffrey@gmail.com>
2022-12-10 11:09:22 -0600

    Day 10 part 1

---
+++ content/post/advent-of-code-2022/day10/computer.py
@@ -0,0 +1,56 @@
+from typing import Iterable
+
+from instructionparser import InstructionParser
+from register import Register
+
+class Computer():
+    def __init__(self, instructions: Iterable[str], parser: InstructionParser = None):
+        self.instructions = instructions
+        self.parser = parser
+
+        self.reg_x = Register(1)
+
+        self.clock_counter = 1
+        self._instruction_index = -1
+        self.advance_instruction()
+        
+
+    def run(self):
+        print("Computer running")
+
+        while self._current_instruction is not None:
+            self.next()
+
+    def run_single_step(self) -> bool:
+        """ Returns True if successfully completed a step, false if halted"""
+
+        if self._current_instruction is not None:
+            self.next()
+            return True
+        else:
+            return False
+
+    def run_until_clock(self, cycles):
+        print(f"Computer running until clock is {cycles}")
+
+        while self.clock_counter < cycles and self._current_instruction is not None:
+            self.next()
+
+    def next(self):
+        if self._current_instruction == None:
+            self._current_instruction = self.parser.parse(next(self.instructions))
+        
+        instruction_complete = self._current_instruction.tick()
+        if (instruction_complete):
+            self._current_instruction.at_complete()
+            
+        self.clock_counter += 1
+    
+    def advance_instruction(self):
+        try:
+            self._current_instruction = self.parser.parse(next(self.instructions), self)
+            self._instruction_index += 1
+            #print(f"Instruction advanced to {self._current_instruction}")
+        except StopIteration:
+            self._current_instruction = None
+        
