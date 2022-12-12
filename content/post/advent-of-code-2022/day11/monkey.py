21cd7431f9c9ae008aae81ad0ad2efd43e0c7a09
Jeff Glass <glass.jeffrey@gmail.com>
2022-12-12 12:03:07 -0600

    Day 11 part 1

---
+++ content/post/advent-of-code-2022/day11/monkey.py
@@ -0,0 +1,49 @@
+from collections import deque
+import re
+from typing import Callable, Sequence
+
+
+class Monkey():
+    def __init__(
+        self,
+        label: int,
+        items: Sequence[int],
+        operation: Callable[[int], int],
+        test: Callable[[int], bool],
+        true_dest: int,
+        false_dest: int
+    ):
+        self.label: int = label
+        self.items = deque(items)
+        self.operation: Callable[[int], int] = operation
+        self.test: Callable[[int], bool] = test
+        self.true_dest: int = true_dest
+        self.false_dest: int = false_dest
+
+        self.throw_count = 0
+
+    def __repr__(self):
+        return f"""Monkey(label={self.label}, \
+items={self.items}, \
+operation={self.operation}, \
+test={self.test}, \
+true_dest={self.true_dest}, \
+false_dest={self.false_dest}), \
+throw_count={self.throw_count}"""
+
+    @classmethod
+    def from_text(cls, text:str):
+        lines: list[str] = text.split('\n')
+        label: int = int(re.match(r'Monkey (?P<label>\d+)',lines[0]).group('label'))
+        starting: list[int] = [int(d) for d in re.findall(r'(\d+)', lines[1])]
+
+        operation = lines[2].removeprefix("  Operation: ").replace("new", "old").replace("=", ":")
+        operation = eval(f"lambda {operation}")
+
+        test_number = int(re.search(r'(\d+)', lines[3]).group())
+        test = lambda x: x % test_number == 0
+
+        to_dest = int(re.search(r'(\d+)', lines[4]).group())
+        false_dest = int(re.search(r'(\d+)', lines[5]).group())
+
+        return cls(label, starting, operation, test, to_dest, false_dest)
