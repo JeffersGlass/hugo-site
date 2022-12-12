675ab7cf78abb26368f3bf6a4042b29f1d5c868e
Jeff Glass <glass.jeffrey@gmail.com>
2022-12-12 08:31:23 -0600

    Pyodide 10.1

--- content/post/advent-of-code-2022/day10/main_1.py
+++ content/post/advent-of-code-2022/day10/main_1.py
@@ -1,15 +1,17 @@
 import sys
+
+if 'pyodide' in sys.modules:
+    import js
+    import os
+    js.console.log(os.listdir('.'))
+
 from typing import Iterable
 
 from computer import Computer
 from parser_10_1 import Parser_10_1
 
-if 'pyodide' in sys.modules:
-    pass
-else:
-    with open('input.txt', 'r') as fp:
-        data = fp.read().split('\n')
 
+def solve_10_1(data):
     comp = Computer(instructions=iter(data), parser=Parser_10_1())
     signal_strength = 0
 
@@ -23,4 +25,16 @@ else:
         signal_strength += signal_value
         #print(f"Running total {signal_strength= }\n")
 
-    print(signal_strength)
+    return(signal_strength)
+
+if 'pyodide' in sys.modules:
+    def main_day10_1():
+        data = get_input('day10_1').split("\n")
+        display(f"{solve_10_1(data)=}",
+            target="day10_1-output",
+            append=False)
+else:
+    with open('input.txt', 'r') as fp:
+        data = fp.read().split('\n')
+
+    print(solve_10_1(data))
