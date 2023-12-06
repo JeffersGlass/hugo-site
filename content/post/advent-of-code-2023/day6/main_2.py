c5491911c0bf8be3f427666f86a1ca3f8d0f9507
Jeff Glass <glass.jeffrey@gmail.com>
2023-12-06 07:55:32 -0600

    Day 6 part 2

--- content/post/advent-of-code-2023/day6/main_2.py
+++ content/post/advent-of-code-2023/day6/main_2.py
@@ -3,16 +3,28 @@ try:
     from utils import get_input
 except ImportError:
     def get_input(*args):
-        with open("input_test.txt") as f:
+        with open("input.txt") as f:
             return f.read()
         
     def display(*args, **kwargs):
         if 'target' in kwargs:
-            dict.pop('target')
+            kwargs.pop('target')
         print(*args, **kwargs)
 
-def main_day6_2(*args):
-    data = get_input("day6_2")
+import re
 
-    result = None
-    display(result, target="day6_2-output")
+def main_day6_2(*args):
+    data = get_input("day6_2").split("\n")
+    total_time = int(''.join(d.group() for d in re.finditer('\d', data[0])))
+    distance_goal = int(''.join(d.group() for d in re.finditer('\d', data[1])))
+
+    count = 0
+    for hold_time in range(total_time):
+        if (total_time - hold_time) * hold_time > distance_goal: count +=1 
+
+    display(count, target="day6_2-output")
+
+try:
+    import js
+except ImportError:
+    main_day6_2()
