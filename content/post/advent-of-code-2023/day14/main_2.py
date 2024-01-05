a2ee7eee58107787e6c214beed5a79257fea3ac3
Jeff Glass <glass.jeffrey@gmail.com>
2024-01-04 20:12:18 -0600

    Tweak part 1

--- content/post/advent-of-code-2023/day14/main_2.py
+++ content/post/advent-of-code-2023/day14/main_2.py
@@ -27,6 +27,15 @@ def completely(data: str, f: [str, str]) -> str:
         new_data = f(data)
     return new_data
 
+def _rollNorth(data: str) -> str:
+    line_num = data.count("\n")+1
+    line_length = data.index("\n")
+    for first in range(line_num) -1:
+        second = first + 1
+        for char_index in range(line_length):
+            if data[char_index + line_length * first] 
+
+
 def rollNorth(data: str) -> str:
     pattern = [list(line) for line in data.split("\n")]
     for first, second in pairwise(pattern):
@@ -71,18 +80,27 @@ def print_pattern(data:str):
     for line in data.split("\n"):
         print(line)
 
+def get_load(data: str) -> int:
+    load = 0
+    for row_index, row in enumerate(data.split("\n")):
+        row_value = len(data.split("\n")) - row_index
+        for char in row:
+            if char == 'O': load += row_value
+    return load
+
 def main_day14_2(*args):
     data = get_input("day14_2")
     configs = []
 
     counter = count()
     while data not in configs:
-        print(loop_point:= counter.__next__())
+        print(loop_point:= counter.__next__(), "\t", get_load(data) )
         configs.append(data)
         data = cycle(data)
 
     loop_point += 1
     first_occurance = configs.index(data)
+    print(f"After {loop_point=} iterations, the stones were in the same place as after {first_occurance}")
     loop_length = loop_point - first_occurance
     target = 1_000_000_000
     last_loop = (int((target - first_occurance)/loop_length)) * loop_length + first_occurance
@@ -90,14 +108,8 @@ def main_day14_2(*args):
 
     for _ in range(difference):
         data = cycle(data)
-        
-    load = 0
-    for row_index, row in enumerate(data.split("\n")):
-        row_value = len(data.split("\n")) - row_index
-        for char in row:
-            if char == 'O': load += row_value
 
-    result = load
+    result = get_load(data)
     display(result, target="day14_2-output")
 
 if not 'js' in sys.modules:
