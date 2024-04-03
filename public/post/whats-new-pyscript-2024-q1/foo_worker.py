61e85d77d72c906a01b942acff8926d6f44700ab
Jeff Glass <glass.jeffrey@gmail.com>
2024-04-03 09:52:24 -0500

    Rebuild public

---
+++ public/post/whats-new-pyscript-2024-q1/foo_worker.py
@@ -0,0 +1,6 @@
+from pyscript import sync
+
+def utility(num):
+    return num * 2
+
+sync.utility = utility
---
+++ public/post/whats-new-pyscript-2024-q1/worker.py
@@ -0,0 +1,13 @@
+from pyscript import sync
+
+def pseudorandom(seed: int, iters: int):
+    """Linear congruential generator. Parameters borrowed from POSIX and are probably bad"""
+    modulus = 2**48
+    a = 25214903917 
+    increment = 11
+
+    for _ in range(iters):
+        seed = (a * seed + increment) % modulus
+    return result
+
+sync.pseudorandom = pseudorandom
